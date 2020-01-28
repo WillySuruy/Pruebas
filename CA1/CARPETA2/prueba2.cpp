3c414b81891e674f5ef4f1a9bffed353326183ca
WillySuruy <willysuruy89@gmail.com>
2020-01-28 13:22:12 -0600

    prueba de git

---
+++ CA1/CARPETA2/prueba2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+cout<<"prueba con git CARPETA 2";
+    return 0;
+}
---
+++ CA1/prueba1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+cout<<"prueba con git CARPETA 1";
+    return 0;
+}
